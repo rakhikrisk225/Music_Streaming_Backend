2205f15860092c63ed4f9a6c3f92df20d4617b05
Rakhi Kumari <rakhikrisk225@gmail.com>
2026-05-29 00:13:26 +0530

    Track Service

---
+++ src/main/java/mini_music_streaming/music_streaming/service/TrackService.java
@@ -0,0 +1,58 @@
+package mini_music_streaming.music_streaming.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mini_music_streaming.music_streaming.entity.TrackEntity;
+import mini_music_streaming.music_streaming.repository.TrackRepository;
+
+@Service
+public class TrackService 
+{
+    @Autowired
+    private TrackRepository trackRepository;  
+    
+    public TrackEntity createTrack(TrackEntity track)
+    {
+        return trackRepository.save(track);
+    }
+
+    public List<TrackEntity> getAllTracks()
+    {
+        return trackRepository.findAll();
+    }
+
+    public TrackEntity getTrackById(Integer Id)
+    {
+        return trackRepository.findById(Id).orElse(null);
+    }
+
+    public String deleteTrack(Integer Id)
+    {
+        trackRepository.deleteById(Id);
+        return "Track Deleted Successfully";
+    } 
+
+    public TrackEntity updateTrack(Integer Id, TrackEntity updatedsong)
+    {
+        TrackEntity existingSong = trackRepository.findById(Id).orElse(null);
+
+        if (existingSong != null) {
+
+            existingSong.setSong(updatedsong.getSong());
+
+            return trackRepository.save(existingSong);
+        }
+
+        return null;
+    }
+
+    public List<TrackEntity> findBySong(String song)
+    {
+        return trackRepository.findBySong(song);
+    }
+
+
+}
