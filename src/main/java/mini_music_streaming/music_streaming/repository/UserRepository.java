dc9998c44aa39df52daf3080de2729854cf80d08
Rakhi Kumari <rakhikrisk225@gmail.com>
2026-05-29 10:26:10 +0530

    User Repository

---
+++ src/main/java/mini_music_streaming/music_streaming/repository/UserRepository.java
@@ -0,0 +1,37 @@
+package mini_music_streaming.music_streaming.repository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import mini_music_streaming.music_streaming.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> 
+{
+
+    public List<UserEntity>findByName(String name);
+
+    public List<UserEntity>findByContact(Long contact);
+     
+    
+    @Query(value = "select * from user",nativeQuery = true)
+    public List<UserEntity>getAllUser();
+
+    @Query(value = "select * from user where name =:name",nativeQuery = true)
+    public List<UserEntity>getUserByName(@Param("name") String name);
+
+    @Query(value="select * from user LIMIT 10",nativeQuery = true)
+    public List<UserEntity>get10user();
+
+    @Modifying
+    @Transactional
+    @Query(value = "insert into user (name, contact, userstatus) VALUES(:name, :contact, :userstatus)",nativeQuery = true)
+    void insertUser(@Param("name") String name,
+                    @Param("contact") Long contact,
+                    @Param("userstatus") String userstatus);
+
+
+}
