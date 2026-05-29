f485e1edd727a18f46665633771bbc9e9944588b
Rakhi Kumari <rakhikrisk225@gmail.com>
2026-05-29 10:27:57 +0530

    Music Streaming Application Test

---
+++ src/test/java/mini_music_streaming/music_streaming/MusicStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package mini_music_streaming.music_streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
